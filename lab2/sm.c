396975b8138f385caea7e053b8f410990a6fcc7e
CJuanvip <e0253722@u.nus.edu>
2020-09-10 21:30:10 +0800

    attempted forking

---
+++ lab2/sm.c
@@ -0,0 +1,64 @@
+/**
+ * CS2106 AY 20/21 Semester 1 - Lab 2
+ *
+ * This file contains function definitions. Your implementation should go in
+ * this file.
+ */
+
+#include "sm.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+// Use this function to any initialisation if you need to.
+void sm_init(void) {
+}
+
+// Use this function to do any cleanup of resources.
+void sm_free(void) {
+}
+
+// Exercise 1a/2: start services
+void sm_start(const char *processes[]) {
+ //   char* array = malloc(sizeof(char)*strlen(processes));
+ //   int PID, i;
+
+ //   array = strcpy(array, processes);
+    int PID;
+    PID = fork();
+
+    if (PID == 0) { //Child Process
+        printf("Child Process! \n");
+
+    }
+
+    else {
+
+        printf("Parent Process! \n");
+    }
+
+
+}//
+
+// Exercise 1b: print service status
+size_t sm_status(sm_status_t statuses[]) {
+}
+
+// Exercise 3: stop service, wait on service, and shutdown
+void sm_stop(size_t index) {
+}
+
+void sm_wait(size_t index) {
+}
+
+void sm_shutdown(void) {
+}
+
+// Exercise 4: start with output redirection
+void sm_startlog(const char *processes[]) {
+}
+
+// Exercise 5: show log file
+void sm_showlog(size_t index) {
+}
