abcb1d36e3f3ecf7a17fbea4f69102c44da563ae
CJuanvip <e0253722@u.nus.edu>
2020-09-15 02:45:44 +0800

    slight progress on ex1

--- lab2/sm.c
+++ lab2/sm.c
@@ -10,6 +10,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
+
+static sm_status_t array[SM_MAX_SERVICES];
+
 
 // Use this function to any initialisation if you need to.
 void sm_init(void) {
@@ -21,16 +25,39 @@ void sm_free(void) {
 
 // Exercise 1a/2: start services
 void sm_start(const char *processes[]) {
-    execv(processes[0], (char**) processes;
+    int PID = fork();
+
+    if (PID == 0) { //Child Process
+        sm_status_t node;
+        malloc(sizeof(node));
+        
+        node.pid = getpid();
+        node.path = (char*) processes;
+        node.running = true;
+
+        array[0] = node;
+
+        execv(processes[0], (char**) processes);
+    }
+
+    wait(NULL);
+
 }
 
 
 // Exercise 1b: print service status
 size_t sm_status(sm_status_t statuses[]) {
+    int i, count;
 
+    for (i=0; i < SM_MAX_SERVICES; i++) {
+       if (array[i] != NULL) {
+            count += 1;
+       }
+    }
 
+    statuses[0] = array[0];
 
-
+    return (size_t) count;
 }
 
 // Exercise 3: stop service, wait on service, and shutdown
