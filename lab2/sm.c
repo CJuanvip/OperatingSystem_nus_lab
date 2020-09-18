24510d005a78a3b29cd1dfb9d493210b7aa74276
CJuanvip <e0253722@u.nus.edu>
2020-09-19 04:35:15 +0800

    Ex2 beginning

--- lab2/sm.c
+++ lab2/sm.c
@@ -30,49 +30,80 @@ void sm_start(const char *processes[]) {
     size_t len = strlen(processes[0]);
     char *src[len];
     *src = (char*) malloc(sizeof(char) * len);     
-    int wstatus;
-    PID = fork();
-    strcpy(*src, processes[0]);
-
-
-    if (PID == 0) { //Child Process
-        execv(processes[0], (char**) processes);
-    }
-
-    else { //Parent Process
-        sm_status_t* node;
-        node = (sm_status_t*) malloc(sizeof(sm_status_t));
+    int wstatus, number_of_pipes = 0, i = 0, j = 1;
+    int positions[SM_MAX_SERVICES];
+    int k;
+    positions[0] = 0;
+   
+    while (true) {
+        printf("%d:", i);
+        if (processes[i] == NULL) {
+            if (processes[i+1] == NULL) {
+                printf("Reached End of Array!");
+                break;     
+            }
+            else {
+                printf("Reached End of SubArray \n");
+                positions[j] = i+1;
+                printf("%d\n", positions[j]);
+                j += 1;
+            }
+            number_of_pipes += 1;
+       }
         
-        w = waitpid(PID, &wstatus, WNOHANG);
-
-        node->pid = PID;
-        node->path =  *src;
-
-        if (w == 0) {
-            node->running = true;
-        }
-
-        else {
-            node->running = false;
-        }
-
-        array[counter] = node;
-        counter += 1;
+       i += 1;
     }
 
+    for (k = 0; k<j; k++) {
+        printf("%d \n", positions[k]);
+    }
+
+//    PID = fork();
+//   // strcpy(*src, processes[0]);
+//
+//
+//    if (PID == 0) { //Child Process
+//        execv(processes[0], (char**) processes);
+//    }
+//
+//    else { //Parent Process
+//        sm_status_t* node;
+//        node = (sm_status_t*) malloc(sizeof(sm_status_t));
+//        
+//        w = waitpid(PID, &wstatus, WNOHANG);
+//
+//        node->pid = PID;
+//        node->path =  *src;
+//
+//        if (w == 0) {
+//            node->running = true;
+//        }
+//
+//        else {
+//            node->running = false;
+//        }
+//
+//        array[counter] = node;
+//        counter += 1;
+//    }
+//
 }
 
 
 // Exercise 1b: print service status
 size_t sm_status(sm_status_t statuses[]) {
     
-   int i;
+   int i, status;
 
    for (i = 0; i < SM_MAX_SERVICES; i++) {
        if (array[i] == NULL) {
            break;
        }
        else {
+           pid_t return_pid = waitpid(array[i]->pid, &status, WNOHANG);
+           if (return_pid != 0) {
+                array[i]->running = false;
+           }
            statuses[i] = *array[i];
        }
    }
