d3a43a86c632e939b0e631c2da2da19bd29f4e8a
CJuanvip <e0253722@u.nus.edu>
2020-09-22 18:01:10 +0800

    ex3 half done

--- lab2/sm.c
+++ lab2/sm.c
@@ -27,6 +27,60 @@ typedef struct vectord {
 static PID_Array* pid_array[SM_MAX_SERVICES];
 static sm_status_t* array[SM_MAX_SERVICES];
 static int counter = 0;
+   
+// Helper function
+void start_helper(const char *processes[], PID_Array* pidarr, int pipeFd[][2], int number_of_pipes, int positions[], int pid_counter) {
+    int k;
+    pid_t PID;
+
+    for (k = 0; k < number_of_pipes; k++) {  
+        if (k == 0 && k != number_of_pipes) { // Check if its the first process and NOT the only process
+
+            pipe(pipeFd[k]);
+                
+            if ((PID = fork()) == 0) { //child process
+                printf("HI CHILDREN\n");    
+
+
+                close(0); //close stdin
+                close(1);
+                dup2(pipeFd[k][WRITE_END], STDOUT_FILENO);
+                execv(processes[positions[k]], (char **) &processes[positions[k]]);
+            }    
+            else {
+                printf("HI PARENT");    
+                pidarr->arr[pid_counter] = PID;
+                pid_counter += 1;
+                wait(NULL);
+                close(pipeFd[k][WRITE_END]);
+            }
+        }
+   
+        else {
+
+            pipe(pipeFd[k]);
+   
+            if ((PID = fork()) == 0) { //child process
+                close(0);
+                close(1);
+                dup2(pipeFd[k-1][READ_END], STDIN_FILENO);
+                dup2(pipeFd[k][WRITE_END], STDOUT_FILENO);
+                   
+                //Right now, both ends of pipe is open
+                execv(processes[positions[k]], (char**) &processes[positions[k]]);
+            }  
+            else {
+                pidarr->arr[pid_counter] = PID;
+                pid_counter += 1;
+                wait(NULL);
+                close(pipeFd[k][WRITE_END]);
+                close(pipeFd[k-1][READ_END]);
+            }
+        }
+     
+    }
+   
+}
 
 
 // Use this function to any initialisation if you need to.
@@ -41,11 +95,12 @@ void sm_free(void) {
 void sm_start(const char *processes[]) {
     pid_t PID, w;
       
-    int wstatus, k, pid_counter = 0, number_of_pipes = 0, i = 0, j = 1;
+    int wstatus;
+    int number_of_pipes = 0, i = 0, j = 1, pid_counter = 0;
     int positions[SM_MAX_SERVICES]; // Array of positions of processes in processes[]
-    PID_Array* pidarr; 
+    static PID_Array* pidarr; 
   
-   
+     
     positions[0] = 0;
     // Getting the posiition of the paths in processes[] and the number of pipes needed
     while (true) { 
@@ -67,42 +122,24 @@ void sm_start(const char *processes[]) {
     pidarr->len = j;
     int pipeFd[number_of_pipes][2];  
 
-    for (k = 0; k < number_of_pipes + 1; k++) { 
-
-        if (k == 0 && k != number_of_pipes) { // At the beginning of the Service
-            
-            pipe(pipeFd[k]);
-
-            if ((PID = fork()) == 0) { //child process
-                close(0); //close stdin
-                close(1);
-                dup2(pipeFd[k][WRITE_END], STDOUT_FILENO);
-                execv(processes[positions[k]], (char **) &processes[positions[k]]);
-            }    
-            else {
-                pidarr->arr[pid_counter] = PID;
-                pid_counter += 1;
-                wait(NULL);
-                close(pipeFd[k][WRITE_END]);
-            }
-        }
-
-        else if (k == number_of_pipes) { // At the end of the Service OR only one process in a service
+    //
+    start_helper(processes, pidarr, pipeFd, number_of_pipes, positions, pid_counter);
+    
             // Acquiring the path for status
-            size_t len = strlen(processes[positions[k]]);
+            size_t len = strlen(processes[positions[j-1]]);
             char *src[len];
             *src = (char*) malloc(sizeof(char) * len);
-            strcpy(*src, processes[positions[k]]);
+            strcpy(*src, processes[positions[j-1]]);
             
 
             if ((PID = fork()) == 0) {
                 close(0);
-                dup2(pipeFd[k - 1][READ_END], STDIN_FILENO);   
-                execv(processes[positions[k]], (char**) &processes[positions[k]]);
+                dup2(pipeFd[j - 2][READ_END], STDIN_FILENO);   
+                execv(processes[positions[j-1]], (char**) &processes[positions[j-1]]);
             }
             else {
-
-                pidarr->arr[pid_counter] = PID;
+                
+                pidarr->arr[j-1]= PID;
        
                 sm_status_t* node;
                 node = (sm_status_t*) malloc(sizeof(sm_status_t));
@@ -124,31 +161,7 @@ void sm_start(const char *processes[]) {
                 array[counter] = node;
                 counter += 1;
            }
-        }
-
-        else {
-            pipe(pipeFd[k]);
-
-            if ((PID = fork()) == 0) { //child process
-                close(0);
-                close(1);
-                dup2(pipeFd[k-1][READ_END], STDIN_FILENO);
-                dup2(pipeFd[k][WRITE_END], STDOUT_FILENO);
-                
-                //Right now, both ends of pipe is open
-                execv(processes[positions[k]], (char**) &processes[positions[k]]);
-            }
-            else {
-                pidarr->arr[pid_counter] = PID;
-                pid_counter += 1;
-                wait(NULL);
-                close(pipeFd[k][WRITE_END]);
-                close(pipeFd[k-1][READ_END]);
-            }
-        }
-    
-    }
-
+        
     int s;
     printf("LIST OF PROCESSES AND THEIR PIDS\n");
     for (s = 0; s < j; s++) {
@@ -213,18 +226,57 @@ size_t sm_status(sm_status_t statuses[]) {
 
 // Exercise 3: stop service, wait on service, and shutdown
 void sm_stop(size_t index) {
+    int length = pid_array[index]->len;
+    int i, w, wstatus;
+
+    for (i = 0; i < length; i++) {
+        w = waitpid(pid_array[index]->arr[i], &wstatus, WNOHANG);
+        
+        if (w == 0) {
+            kill(pid_array[index]->arr[i], 15);
+        }
+    }
 }
 
 void sm_wait(size_t index) {
+    int length = pid_array[index]->len;
+    int i, w, wstatus;
+
+    for (i = 0; i < length; i++) {
+        w = waitpid(pid_array[index]->arr[i], &wstatus, WNOHANG);
+        
+        if (w == 0) {
+            kill(pid_array[index]->arr[i], 15);
+        }
+    }
+
 }
 
 void sm_shutdown(void) {
+    int i;
+
+    for (i = 0; i < SM_MAX_SERVICES; i++) {
+        if (array[i] == NULL) {
+            break;
+        }
+        else {
+            sm_stop(i);       
+        }
+    }
 }
 
 // Exercise 4: start with output redirection
 void sm_startlog(const char *processes[]) {
+
+    
 }
 
 // Exercise 5: show log file
 void sm_showlog(size_t index) {
+
+
+
+
+
+
 }
